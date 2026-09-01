7acbbd3d5a39cbaf6c57b2aa7bbf54dcbe7d99f7
pongkaew-udom <pongkaew@g.swu.ac.th>
2026-09-01 08:20:16 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11538113
+    "zoneId": 11698359
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
